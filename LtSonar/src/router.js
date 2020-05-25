357ba4232300d69abb347ac1b33daaddeb5974e6
Jonathan Damiani <jdeandamianiapps@gmail.com>
2020-05-25 16:04:42 -0700

    First components

--- LtSonar/src/router.js
+++ LtSonar/src/router.js
@@ -10,11 +10,18 @@ Vue.use( Router );
 
 import Home from '@/routes/Home.vue'
 import About from '@/routes/About.vue'
-
+import lsCaptain from '@/routes/Captain.vue'
+import lsNavigator from '@/routes/Navigator.vue'
+import lsFirstOfficer from '@/routes/FirstOfficer.vue'
+import lsEngineer from '@/routes/Engineer.vue'
 
 export default new Router({
     routes: [
-        { path:"/",     name:"Root", component: About },
-        { path:"/home", name:"Home", component: Home, props: { name: "DemoApp"} },
+        { path:"/",                 name:"Lobby", component: About },
+        { path:"/home",             name:"Home", component: Home, props: { name: "DemoApp"} },
+        { path:"/navigator",        name:"Navigator", component: lsNavigator},
+        { path:"/captain",          name:"Captain", component: lsCaptain},
+        { path:"/firstofficer",     name:"Firstofficer", component: lsFirstOfficer },
+        { path:"/engineer",         name:"Engineer", component: lsEngineer },
     ]
 });
